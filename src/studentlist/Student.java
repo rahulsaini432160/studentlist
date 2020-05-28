161a59f966a4f0bbee42c14155d6b0e1ebb86f9d
rahul <rahul@192.168.0.17>
2020-05-28 19:27:59 -0400

    new variable added

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -8,6 +8,7 @@ package studentlist;
 public class Student {
 
     private String name;
+    private String address;
 
     public Student(String name) {
         this.name = name;
@@ -15,6 +16,9 @@ public class Student {
 
     public String getName() {
         return name;
+    }
+    public String getAddress(){
+        return address;
     }
 
     public void setName(String name) {
